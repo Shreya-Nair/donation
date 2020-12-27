f29c44a750b4eb567bf1b8969c4caff169f1af96
Shreya Nair <shreyanair62@gmail.com>
2020-12-27 12:22:10 +0530

    added new pages

---
+++ app/src/main/java/com/example/donation/MainActivity_donor1.kt
@@ -0,0 +1,26 @@
+package com.example.donation
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity_donor1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_donor1)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+
+            val intent = Intent(this,MainActivity_donor2::class.java)
+            startActivity(intent)
+        }
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+
+            val intent = Intent(this,MainActivity_donor3::class.java)
+            startActivity(intent)
+        }
+    }
+}
